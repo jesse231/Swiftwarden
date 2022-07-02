889a073e47a57181f6f0628307e7bf5e6847e8cf
Jesse Seeligsohn <jesseseeligsohn@gmail.com>
2022-07-02 17:56:05 -0400

    Initial Commit

---
+++ Bitwarden/BitwardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitwardenApp.swift
+//  Bitwarden
+//
+//  Created by Jesse Seeligsohn on 2022-07-02.
+//
+
+import SwiftUI
+
+@main
+struct BitwardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bitwarden/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bitwarden
+//
+//  Created by Jesse Seeligsohn on 2022-07-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
