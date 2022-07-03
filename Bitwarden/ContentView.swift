b3d023787e495baf046ee8865a7dab6af39e5cdf
Jesse Seeligsohn <jesseseeligsohn@gmail.com>
2022-07-03 15:24:12 -0400

    Changes navigation style to NavigationLink (hardcoded) and creates a view for each item

--- Bitwarden/ContentView.swift
+++ Bitwarden/ContentView.swift
@@ -8,51 +8,89 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    @State var currentOption = 0
     var body: some View {
         NavigationView{
-            let options: [Option] = [
-                .init(title:"All Items", icon: "house"),
-                .init(title:"Favourites", icon: "star"),
-                .init(title:"Trash", icon: "trash.fill"),
-            ]
-            SideView(options: options)
-        
-        MainView()
+            
+//            let options: [Option] = [
+//                .init(label:"All Items", icon: "house", destination: MainView),
+//                .init(label:"Favourites", icon: "star", ),
+//                .init(label:"Trash", icon: "trash.fill"),
+//            ]
+            
+            SideBar()
+            
+            
+//
+//            SideView(options: options, currentSelection : $currentOption)
+            
+//            switch currentOption {
+//            case 1:
+//                MainView()
+//            default:
+//                Text("testing")
+//
+//            }
+            
         }
     }
 }
-
-struct Option : Hashable {
-    let title: String
-    let icon: String
-}
+//
+//struct Option : Hashable {
+//    let label: String
+//    let icon: String
+//}
     
-struct SideView: View {
-    
-    let options: [Option]
+struct SideBar: View {
     
     var body: some View {
-        VStack {
-            ForEach(options, id:\.self) { option in
-                HStack {
-                    Image(systemName: option.icon)
-                    Text(option.title)
-                    Spacer()
-                }.padding(6)
-            }
-        }.padding()
-        Spacer()
+        VStack{
+            Spacer().frame(height:10)
+        List{
+                NavigationLink(
+                    destination: ItemsView(),
+                    label: {
+                    Label("All Items", systemImage: "shield.lefthalf.fill")
+                   })
+            
+            NavigationLink(
+                destination: FavouritesView(),
+                label: {
+                Label("Favourites", systemImage: "star.fill")
+               })
+            
+            NavigationLink(
+                destination: TrashView(),
+                label: {
+                Label("Trash", systemImage: "trash.fill")
+               })
+            }.listStyle(.sidebar)
+    }
     }
 }
-struct MainView: View {
+struct ItemsView: View {
+    var body: some View {
+        Text("test")
+        
+    }
+}
+struct FavouritesView: View {
+    var body: some View {
+        Text("test")
+        
+    }
+}
+struct TrashView: View {
     var body: some View {
         Text("test")
         
     }
 }
 
+
+
 struct ContentView_Previews: PreviewProvider {
+    
     static var previews: some View {
         ContentView()
     }
