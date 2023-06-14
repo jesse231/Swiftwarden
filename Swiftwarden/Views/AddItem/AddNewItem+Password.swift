9cf8f1ae0f374e9b1d67ed1a6bd82403b8ee11b4
Jesse Seeligsohn <jesseseeligsohn@gmail.com>
2023-06-13 23:19:49 -0400

    Put customfields in addpassword and style

--- Swiftwarden/Views/AddItem/AddNewItem+Password.swift
+++ Swiftwarden/Views/AddItem/AddNewItem+Password.swift
@@ -10,7 +10,7 @@ import SwiftUI
 extension AddNewItemPopup {
     struct AddPassword: View {
         var account: Account
-        @State var name = ""
+        @Binding var name: String
         @State var username = ""
         @State var password = ""
         
@@ -18,72 +18,58 @@ extension AddNewItemPopup {
         @State var reprompt = false
         
         @State var uris: [Uris] = [Uris(url: "")]
-        @State var selectedFolder: Folder = Folder(name: "")
+        @State var fields: [CustomField] = []
+        @State var selectedFolder: Folder
+        init(account: Account, name: Binding<String>) {
+            self.account = account
+            self._name = name
+            _selectedFolder = State(initialValue: account.user.getFolders()[0])
+        }
+        
         var body: some View {
-            VStack{
-                GroupBox {
-                    TextField("Name", text: $name)
-                        .textFieldStyle(.plain)
-                        .padding(8)
+                VStack{
+                    GroupBox {
+                        TextField("Name", text: $name)
+                            .textFieldStyle(.plain)
+                            .padding(8)
+                    }
+                    .padding(.bottom, 4)
+                    GroupBox {
+                        TextField("Username", text: $username)
+                            .textFieldStyle(.plain)
+                            .padding(8)
+                    }.padding(.bottom, 4)
+                    GroupBox {
+                        SecureField("Password", text: $password)
+                            .textFieldStyle(.plain)
+                            .padding(8)
+                    }.padding(.bottom, 12)
+                    Divider()
+                    AddUrlList(urls: $uris)
+                    Divider()
+                    CustomFields(fields: $fields)
+                    Divider()
+//                    VStack {
+                        Form {
+                            Picker("Folder", selection: $selectedFolder) {
+                                ForEach(account.user.getFolders(), id: \.self) {folder in
+                                    Text(folder.name)
+                                }
+                                
+                            }
+                            Toggle("Favorite", isOn: $favorite)
+                            Toggle("Master password re-prompt", isOn: $reprompt)
+                        }
+                        .scaledToFit()
+                        .scaledToFill()
+                        .scrollDisabled(true)
+                        .formStyle(.grouped)
+                        .scrollContentBackground(.hidden)
+//                    }
+//                    .background(.red)
+//                    Spacer()
+//                        .padding(.bottom)
                 }
-                .padding(.bottom, 4)
-                GroupBox {
-                    TextField("Username", text: $username)
-                        .textFieldStyle(.plain)
-                        .padding(8)
-                }.padding(.bottom, 4)
-                GroupBox {
-                    SecureField("Password", text: $password)
-                        .textFieldStyle(.plain)
-                        .padding(8)
-                }.padding(.bottom, 12)
-                Divider()
-                AddUrlList(urls: $uris)
-                Divider()
-                    .padding(.bottom, 12)
-                VStack {
-                    HStack {
-                        Picker(selection: $selectedFolder, content: {
-                            ForEach(account.user.getFolders(), id: \.self) {folder in
-                                Text(folder.name)
-                            }
-                            
-                        }) {
-                            Text("Folder")
-                                .foregroundColor(.gray)
-                                .padding(.trailing, 300)
-                        }
-                    }
-                    HStack {
-                        Text("Favorite")
-                            .frame(alignment: .trailing)
-                            .foregroundColor(.gray)
-                        Spacer()
-                        Toggle("Favorite", isOn: $favorite).labelsHidden()
-                    }
-                    
-                    HStack {
-                        Text("Master Password Re-prompt")
-                            .frame(alignment: .trailing)
-                            .foregroundColor(.gray)
-                        Spacer()
-                        Toggle("Reprompt", isOn: $reprompt).labelsHidden()
-                    }
-                    .padding(.bottom, 20)
-                    HStack {
-                        Picker(selection: $selectedFolder, content: {
-                            ForEach(account.user.getFolders(), id: \.self) {folder in
-                                Text(folder.name)
-                            }
-                            
-                        }) {
-                            Text("Owner")
-                                .foregroundColor(.gray)
-                                .padding(.trailing, 300)
-                        }
-                    }
-                }.padding(.bottom, 24)
-            }
         }
     }
 }
--- Swiftwarden/Views/AddItem/AddNewItemPopup.swift
+++ Swiftwarden/Views/AddItem/AddNewItemPopup.swift
@@ -4,36 +4,80 @@ struct AddNewItemPopup: View {
     @EnvironmentObject var account: Account
     @Binding var show: Bool
     @State var name = ""
-    @State var username = ""
-    @State var password = ""
-    @State var url = ""
-    @State var folder = "Server"
-    @State var favorite = false
-    @State var reprompt = false
-    @State var uris: [Uris] = [Uris(url: "")]
     
     @State var itemType: ItemType
     
     var body: some View {
-        switch itemType {
-        case .password:
-            AddPassword(account: account)
-                .padding()
-                .frame(width: 500, height: 500)
-        case .card:
-            Text("Card")
-        case .identity:
-            Text("identity")
-        case .folder:
-            Text("folder")
+        VStack {
+            if name.count != 0{
+                Text(name).font(.title).bold()
+            } else {
+                Text("New Password").font(.title).bold()
+            }
+            Divider()
+            ScrollView {
+                switch itemType {
+                case .password:
+                    AddPassword(account: account, name: $name)
+                        .padding()
+                        .frame(width: 500)
+                case .card:
+                    Text("Card")
+                case .identity:
+                    Text("identity")
+                case .folder:
+                    Text("folder")
+                }
+            }
+            HStack{
+                Button {
+                    show = false
+                    
+                } label: {
+                    Text("Cancel")
+                }
+                Spacer()
+                Button {
+                    Task {
+                        //                    let url = uris.first?.uri
+                        //                    let newCipher = Cipher(
+                        //                        favorite: favorite,
+                        //                        fields: nil,
+                        //                        folderID: selectedFolder.id != "No Folder" ? selectedFolder.id : nil,
+                        //
+                        //                        login: Login(
+                        //                            password: password != "" ? password : nil,
+                        //                            uri: url,
+                        //                            uris: uris,
+                        //                            username: username != "" ? username : nil),
+                        //                        name: name,
+                        //                        reprompt: reprompt ? 1 : 0,
+                        //                        type: 1
+                        //                    )
+                        //                    do {
+                        //                        self.account.selectedCipher =
+                        //                        try await account.user.addCipher(cipher: newCipher, api: account.api)
+                        //                    }
+                        //                    catch {
+                        //                        print(error)
+                        //                    }
+                        //
+                        //                }
+                        //                show = false
+                    }
+                    } label: {
+                        Text("Save")
+                    }
+                }
+            }
+        .padding()
+        .frame(width: 500, height: 500)
+    }
+}
+        struct AddNewItemPopup_Previews: PreviewProvider {
+            @State static var show = true
+            var account: Account = Account()
+            static var previews: some View {
+                AddNewItemPopup(show: $show, itemType: .password).environmentObject(Account())
+            }
         }
-    }
-}
-
-struct AddNewItemPopup_Previews: PreviewProvider {
-    @State static var show = true
-    var account: Account = Account()
-    static var previews: some View {
-        AddNewItemPopup(show: $show, itemType: .password).environmentObject(Account())
-    }
-}
