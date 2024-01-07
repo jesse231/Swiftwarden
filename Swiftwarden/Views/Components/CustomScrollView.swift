52744d113f194e1af29fc29603fbeb05e9bf971d
Jesse Seeligsohn <jesseseeligsohn@gmail.com>
2024-01-07 16:25:37 -0500

    Created custom scrollview for better UI

---
+++ Swiftwarden/Views/Components/CustomScrollView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomScrollView.swift
+//  Swiftwarden
+//
+//  Created by Jesse Seeligsohn on 2023-07-31.
+//
+
+import SwiftUI
+
+struct CustomScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomScrollView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomScrollView()
+    }
+}
